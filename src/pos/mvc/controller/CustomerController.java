7d73bd42165945d04494e8cdd49217af3120285f
unknown <bimsaranirmal123@gmail.com>
2023-07-30 14:10:26 +0530

    first commit

---
+++ src/pos/mvc/controller/CustomerController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pos.mvc.controller;
+
+
+
+import pos.mvc.model.CustomerModel;
+/**
+ *
+ * @author BIMSARA
+ */
+public class CustomerController {
+    public String saveCustomer(CustomerModel customer){
+        
+        return null;
+        
+    }
+}
