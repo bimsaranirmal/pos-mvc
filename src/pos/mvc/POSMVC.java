e4a05fd9157f81ab0dc69f75a563d6ea15963915
unknown <bimsaranirmal123@gmail.com>
2023-08-03 19:17:21 +0530

    place order

--- src/pos/mvc/POSMVC.java
+++ src/pos/mvc/POSMVC.java
@@ -6,6 +6,7 @@ package pos.mvc;
 
 import pos.mvc.view.CustomerView;
 import pos.mvc.view.ItemView;
+import pos.mvc.view.OrderView;
 
 /**
  *
@@ -18,7 +19,7 @@ public class POSMVC {
      */
     public static void main(String[] args) {
         //new CustomerView().setVisible(true);
-        new ItemView().setVisible(true);
+        new OrderView().setVisible(true);
     }
     
 }
